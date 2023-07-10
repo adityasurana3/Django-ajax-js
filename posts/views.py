588185438b72c54a9221ca402f1ab1b6e6453986
Aditya Surana <suranaaditya3@gmail.com>
2023-07-10 21:48:20 +0530

    like unlike functionality added

--- posts/views.py
+++ posts/views.py
@@ -11,15 +11,34 @@ def post_list_and_create(request):
 def ajax_hello_world(request):
     return JsonResponse({'text':'Hello World'})
 
-def load_post_data_views(request):
+def load_post_data_views(request, num_posts):
+    visible = 3
+    upper = num_posts
+    lower = upper - visible
+    size = Post.objects.all().count()
     datas = Post.objects.all()
     data_list = []
     for data in datas:
         data_dict = {
             'id':data.id,
-            'author':data.author.user.username,
             'title':data.title,
-            'body': data.body
+            'body': data.body,
+            'liked': True if request.user in data.likes.all() else False,
+            'count': data.like_count,
+            'author':data.author.user.username,
         }
         data_list.append(data_dict)
-    return JsonResponse({'data_list':data_list})
+    return JsonResponse({'data_list':data_list[lower:upper],'size':size})
+
+
+def like_unlike_post(request):
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if request.user in obj.likes.all():
+            liked = False
+            obj.likes.remove(request.user)
+        else:
+            liked = True
+            obj.likes.add(request.user)
+        return JsonResponse({'liked':liked,'count':obj.like_count})
--- static/js/posts.js
+++ static/js/posts.js
@@ -1,5 +1,7 @@
-const hello = document.querySelector('#hello-world')
+const post = document.querySelector('#posts-box')
 const spinnerBox = document.querySelector('#spinner-box')
+const loadBtn = document.getElementById("load-btn")
+const endBox = document.getElementById("end-box")
 
 console.log("Hello WOrld")
 // $.ajax({
@@ -12,17 +14,101 @@ console.log("Hello WOrld")
 //         hello.innerHTML = err;
 //     }
 // })
-$.ajax({
-    type: "GET",
-    url: "data/",
-    success: function(response){
-        const data=response.data_list
-        spinnerBox.classList.add('not-visible')
-        data.forEach(element => {
-            hello.innerHTML += `<p>${element.title} - ${element.body}</p>`
-        });
-    },
-    error: function(error){
-        console.log(error);
+
+
+const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
     }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+const likeUnlikePosts = (e) =>{
+    const likeUnkileForms = [...document.getElementsByClassName('like-unlike-forms')]
+    likeUnkileForms.forEach(form=> form.addEventListener('submit',e=>{
+        e.preventDefault();
+        const clickedId = e.target.getAttribute('data-form-id')
+        const clickedBtn = document.getElementById(`like-unlike-${clickedId}`)
+
+        $.ajax({
+            type: "POST",
+            url: "/like-unlike/",
+            data: {
+                'csrfmiddlewaretoken':csrftoken,
+                'pk':clickedId,
+            },
+            success: function(response){
+                clickedBtn.textContent = response.liked ? `Unkile(${response.count})`: `Like(${response.count})`
+            },
+            error: function(error){
+                console.log(error);
+            }
+        })
+
+    }))
+}
+
+
+const getData = () =>{
+    $.ajax({
+        type: "GET",
+        url: `data/${visible}`,
+        success: function(response){
+            const data=response.data_list
+            spinnerBox.classList.add('not-visible')
+            data.forEach(element => {
+                post.innerHTML += `
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text">${element.body}</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-1">
+                                <a href="#" class="btn btn-primary">Details</a>
+                            </div>
+                            <div class="col-1">
+                                <form class="like-unlike-forms" data-form-id="${element.id}">
+                                    <button class="btn btn-primary" id="like-unlike-${element.id}">${element.liked ? `Unkile(${element.count})`: `Like(${element.count})`}</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `
+            });
+            likeUnlikePosts();
+            if(response.size === 0){
+                endBox.innerHTML = "No post added yet"
+            }
+            else if(response.size <= visible){
+                loadBtn.classList.add("not-visible")
+                endBox.innerHTML = "No more post to load..."
+            }
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
+}
+
+loadBtn.addEventListener('click',()=>{
+    spinnerBox.classList.remove("not-visible")
+    visible += 3;
+    getData()
 })
+
+let visible = 3;
+
+getData();
